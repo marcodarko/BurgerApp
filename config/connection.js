fdd5d383e6b24687ad3f9efe299771cf8ebebaff
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-04-28 16:09:10 -0700

    pool

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,42 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "us-cdbr-iron-east-03.cleardb.net",
-  user: "b204e93cac7161",
-  password: "cad08e55",
-  database: "heroku_fcbb3d9e4d63f93",
-  connectionLimit: 5
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "us-cdbr-iron-east-03.cleardb.net",
+//   user: "b204e93cac7161",
+//   password: "cad08e55",
+//   database: "heroku_fcbb3d9e4d63f93",
+//   connectionLimit: 5
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+
+
+var mysql = require('mysql');
+
+var pool  = mysql.createPool({
+  connectionLimit : 5,
+  host            : 'us-cdbr-iron-east-03.cleardb.net',
+  user            : 'b204e93cac7161',
+  password        : 'cad08e55',
+  database        : 'heroku_fcbb3d9e4d63f93'
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
 });
 
-// Export connection for our ORM to use.
-module.exports = connection;
-
+module.exports= pool;
