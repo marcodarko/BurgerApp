d33b60102de7f73b3a4d5bcb387941502865560d
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-04-28 00:33:26 -0700

    route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.selectAll('burgers',function(data) {
     var hbsObject = {
       burgers: data
     };
